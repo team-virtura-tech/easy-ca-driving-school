106471c7e459124fd578e5096bbf2ccab28015fd
Pritesh Patel <pritesh.patel52@outlook.com>
2025-08-10 20:49:18 -0700

    add landing page 2

---
+++ src/app/landing-2/page.tsx
@@ -0,0 +1,10 @@
+import { Hero2 } from '@/components/custom/hero2';
+
+export default function Home() {
+  return (
+    <div className="min-h-screen">
+      <Hero2 />
+      {/* Future sections can be added here */}
+    </div>
+  );
+}
---
+++ src/components/custom/hero2/index.ts
@@ -0,0 +1,2 @@
+export { Hero2 } from './hero2';
+export type { HeroProps } from './hero2';
